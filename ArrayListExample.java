7ae0c8ec396be3dce7802ab4a3ac1238d820edf4
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-12-22 22:08:28 +0530

    Array Lizt Example

--- ArrayListExample.java
+++ ArrayListExample.java
@@ -3,7 +3,6 @@ class Book{
     int id;
     String name,author,publisher;
     int quantity;
-    
     public Book(int id, String name, String author, String publisher, int quantity){
         this.id= id;
         this.name =name;
